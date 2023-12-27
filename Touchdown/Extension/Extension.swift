5e1fd01fa7baa827d37b049445ce5d2c9fb1d280
joricky91 <jonathanricky91@gmail.com>
2023-12-27 14:54:57 +0700

    feat: finishing custom navbar

---
+++ Touchdown/Extension/Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Extension.swift
+//  Touchdown
+//
+//  Created by Jonathan Ricky Sandjaja on 27/12/23.
+//
+
+import Foundation
---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Jonathan Ricky Sandjaja on 27/12/23.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
